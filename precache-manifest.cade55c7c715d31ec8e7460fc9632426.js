b55dccd5458041c73378b0693653fc8a3ed153a6
asilva-cell <achapellin@gmail.com>
2020-05-06 15:24:13 -0600

    Updates

---
+++ precache-manifest.cade55c7c715d31ec8e7460fc9632426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ff8789dcee3e57c4a26f0dc6813960",
+    "url": "/monstersApp/index.html"
+  },
+  {
+    "revision": "1b7f47619189221c5963",
+    "url": "/monstersApp/static/css/main.111c8f69.chunk.css"
+  },
+  {
+    "revision": "586d1936e110df5acc4d",
+    "url": "/monstersApp/static/js/2.57b326a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersApp/static/js/2.57b326a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7f47619189221c5963",
+    "url": "/monstersApp/static/js/main.272d00c8.chunk.js"
+  },
+  {
+    "revision": "43cd47d3bd2ba85cd41e",
+    "url": "/monstersApp/static/js/runtime-main.de7eaedb.js"
+  }
+]);
